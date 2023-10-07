c4decbbd51d80274afb9db6315f2058ef5994cd0
Watheq Attrah <novaatrah@gmail.com>
2023-10-07 14:44:29 -0700

    Update all client components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import SignupForm from './components/Signup';
+import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import NavBar from "./component/NavBar"
+import Home from "./component/Home"
+import Post from './component/Post'
+import Login from './component/Login'
+import Signup from './component/Signup'
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+
+  useEffect(() => {
+    fetch("/check_session")
+      .then((response) => {
+        if (response.status === 200) {
+          response.json().then((user) => setUser(user));
+        }
+      });
+  }, [])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <SignupForm />
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <NavBar user={user} setUser={setUser} />
+        <main>
+          {user ? (
+            <Switch>
+              <Route path='/'>
+                <Home user={user} />
+                <Post user={user} />
+              </Route>
+            </Switch>
+          ) : (
+            <Switch>
+              <Route path='/signup'>
+                <Signup setUser={setUser} />
+              </Route>
+              <Route path='/login'>
+                <Login setUser={setUser} />
+              </Route>
+              <Route NewPost='/Post'>
+                <Home />
+              </Route>
+              <Route path='/'>
+                <Home />
+              </Route>
+            </Switch>
+          )
+          }
+        </main>
       </header>
     </div>
   );
